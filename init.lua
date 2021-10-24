a03a27f4f58be9374ccbd9d1cf0508bd70cb85db
Johan Palacios <johanpalacios.rivera@gmail.com>
2021-10-24 16:47:16 -0600

    init.vim to init.lua

---
+++ init.lua
@@ -0,0 +1,18 @@
+--  +----------------------------+
+--   |                            |
+--   |Jᴏʜᴀɴ Pᴀʟᴀᴄɪᴏs /NVIM-Cᴏɴғɪɢ |
+--   |         I love NVIM        |
+--   |   This is for NVIM 0.5_V   |
+--   |                            |
+--   +----------------------------+
+--    _  ____
+--   / |/  __\     GitHub: https://github.com/Johan-Palacios
+--   | ||  \/|     Youtube: www.youtube.com/channel/UC8h9RRhxtAbpE3-J3RQljKQ
+--/\_| ||  __/     IG: www.instagram.com/_el_johan/
+--\____/\_/        FB: www.facebook.com/johan.palacios.fx/
+--                 I use :                  ﬏
+require 'init'
+require('impatient')
+require'impatient'.enable_profile()
+--themes
+vim.cmd("source $HOME/.config/nvim/themes/onedark.vim")

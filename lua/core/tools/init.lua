d44b68425d80c206fa75a741050b1207198d5eec
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-05-31 20:33:18 -0600

    Modify and change name of files

---
+++ lua/core/tools/init.lua
@@ -0,0 +1,22 @@
+-- NOTE: TOOLS PLUGINS CONFIG
+require "core.tools.trouble"
+
+require "core.tools.scroll"
+
+require "core.tools.autopairs"
+
+require "core.tools.numb"
+
+require "core.tools.autosave"
+
+require "core.tools.fileicon"
+
+require "core.tools.luasnip"
+
+require "core.tools.tmux"
+
+require "core.tools.sesion"
+
+require "core.tools.jabs"
+
+require "core.tools.jaq"

9e5710d58cb1ec789202aa1432c1375e5c967c42
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-11 20:58:49 -0600

    `gq` for range format enable

--- lua/lsp/handler.lua
+++ lua/lsp/handler.lua
@@ -81,8 +81,8 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gn", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
   vim.api.nvim_create_user_command("Format", function()
-    vim.lsp.buf.format { async = true }
-  end, {})
+    vim.lsp.buf.format { async = true, bufnr = bufnr }
+  end, { range = 0 })
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>lf", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>lr", "<cmd>Lspsaga rename<CR>", { silent = true })
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>ld", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
@@ -100,6 +100,11 @@ M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
   require("nvim-navic").attach(client, bufnr)
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
+
+  buf_set_option("formatexpr", "v:lua.vim.lsp.formatexpr(#{timeout_ms:250})")
   if client.name == "tsserver" then
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -24,6 +24,7 @@ local servers = {
   "cmake",
   "clangd",
   "tsserver",
+  "tailwindcss"
 }
 
 local settings = {

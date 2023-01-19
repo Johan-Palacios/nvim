ab56a53f841b4be4112ae54c38d6486033b687cc
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-18 20:34:22 -0600

    Remove packer support in initial files

--- lua/init.lua
+++ lua/init.lua
@@ -1,12 +1,6 @@
 require "configs.options"
 
--- Ensure packer compiled
-local is_compiled = pcall(require, "packer_compiled")
-if is_compiled then
-  require "packer_compiled"
-end
-
-require "plugins"
+require "plugins-loader"
 
 require "lsp.init"
 

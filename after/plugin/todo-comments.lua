495285e82c1e3e20b0b5e585882791f82450e5fe
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-31 20:16:15 -0600

    Update Todo-comments

--- after/plugin/todo-comments.lua
+++ after/plugin/todo-comments.lua
@@ -2,10 +2,7 @@ local status_ok, todo = pcall(require, "todo-comments")
 if not status_ok then
   return
 end
-local colors = {
-  purple = "#b273c0",
-  grey = "#abb2bf"
-}
+
 todo.setup {
   signs = true,
   sign_priority = 8,
@@ -13,31 +10,29 @@ todo.setup {
     FIX = {
       icon = " ",
       color = "error",
-      alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }
+      alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
     },
     TODO = {
       icon = " ",
-      color = "info"
+      color = "info",
     },
     HACK = {
       icon = " ",
-      color = "warning"
+      color = "warning",
     },
     WARN = {
       icon = " ",
       color = "warning",
-      alt = { "WARNING", "XXX" }
+      alt = { "WARNING", "XXX" },
     },
     PERF = {
       icon = " ",
-      color = colors.grey,
-      alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" }
+      alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" },
     },
     NOTE = {
       icon = " ",
-      color = colors.purple,
-      alt = { "INFO" }
-    }
+      alt = { "INFO" },
+    },
   },
   merge_keywords = true,
   highlight = {
@@ -47,21 +42,24 @@ todo.setup {
     pattern = [[.*<(KEYWORDS)\s*:]],
     comments_only = true,
     max_line_len = 400,
-    exclude = {}
+    exclude = {},
   },
   colors = {
-    error = { "LspDiagnosticsDefaultError", "ErrorMsg", "#DC2626" },
-    warning = { "LspDiagnosticsDefaultWarning", "WarningMsg", "#FBBF24" },
-    info = { "LspDiagnosticsDefaultInformation", "#2563EB" },
-    hint = { "LspDiagnosticsDefaultHint", "#10B981" },
-    default = { "Identifier", "#7C3AED" }
+    error = { "LspDiagnosticsDefaultError" },
+    warning = { "LspDiagnosticsDefaultWarning" },
+    info = { "LspDiagnosticsDefaultInformation" },
+    hint = { "LspDiagnosticsDefaultHint" },
+    default = { "Identifier" },
   },
   search = {
     command = "rg",
     args = {
-      "--color=never", "--no-heading", "--with-filename", "--line-number",
-      "--column"
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
     },
-    pattern = [[\b(KEYWORDS)\b]]
-  }
+    pattern = [[\b(KEYWORDS)\b]],
+  },
 }

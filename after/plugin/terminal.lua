2c63fbc29498abc4793a32c359504cb7429ca3f2
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-31 20:15:57 -0600

    Terminal update

--- after/plugin/terminal.lua
+++ after/plugin/terminal.lua
@@ -2,24 +2,34 @@ local status_ok, term = pcall(require, "toggleterm")
 if not status_ok then
   return
 end
-term.setup({
-	size = 50,
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 3,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	-- direction = 'vertical' | 'horizontal' | 'window' | 'float',
-	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
-	},
-})
+
+local get_hl = require("core.functions").get_hlgroup
+
+term.setup {
+  size = 50,
+  hide_numbers = true,
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = 3,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  -- direction = 'vertical' | 'horizontal' | 'window' | 'float',
+  direction = "float",
+  close_on_exit = true,
+  shell = vim.o.shell,
+  highlights = {
+    Normal = {
+      guifg = get_hl("Normal").fg,
+      guibg = get_hl("Normal").bg,
+    },
+    NormalFloat = {
+      link = 'Normal'
+    },
+    FloatBorder = {
+      guibg = get_hl("Normal").bg,
+      guifg = get_hl("FloatBorder").fg,
+    },
+  },
+}
+

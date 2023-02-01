9b538acc4266206c53622af0c8a3d7a8a3f2d675
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-31 20:17:55 -0600

    Update TREE for resent update

--- lua/core/tree.lua
+++ lua/core/tree.lua
@@ -43,19 +43,12 @@ tree.setup {
   },
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = { "dashboard" },
-  open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
   update_focused_file = {
     enable = true,
     update_cwd = true,
     ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
   },
   filters = {
     dotfiles = false,

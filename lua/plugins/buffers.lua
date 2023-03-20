81deb723beac42f485fc0437bc22ccc62348f8cf
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-03-19 22:45:34 -0600

    JABS to `plugins/buffers.lua`

---
+++ lua/plugins/buffers.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "matbme/JABS.nvim",
+    opts = {
+      position = "center",
+      width = 50,
+      height = 10,
+      border = "rounded",
+      preview_position = "top",
+      preview = {
+        width = 70,
+        height = 20,
+        border = "rounded",
+      },
+      keymap = {
+        close = "d",
+        jump = "<space>",
+        h_split = "h",
+        v_split = "v",
+        preview = "p",
+      },
+      use_devicons = true,
+    },
+  },
+}

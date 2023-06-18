1d2263e981bc19e389b767455100eadc8851f3cf
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-06-17 22:17:06 -0600

    feat: Refactor CMP and add docs

--- lua/core/cmp.lua
+++ lua/core/cmp.lua
@@ -8,10 +8,10 @@ if not snip_status_ok then
   return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
-
+---Check backspace to improve Auto Complete
+---@return boolean
 local check_backspace = function()
----@diagnostic disable-next-line: deprecated
+  ---@diagnostic disable-next-line: deprecated
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
@@ -19,10 +19,14 @@ end
 ---Ignore files
 ---@return boolean
 local ignore_typefile = function()
+  --Set ignore filetypes
   local ignore = { "java" }
+
+  -- If the file type is equals to current filetype returns true
   for _, value in ipairs(ignore) do
     return vim.bo.filetype == value
   end
+
   return false
 end
 
@@ -32,10 +36,13 @@ end
 ---@param lsptext string
 ---@return string
 local improved_menu = function(menu, entry, lsptext)
+  -- Checks that the file is in the ignore list, set detail into a empty string
   if ignore_typefile() then
     entry.completion_item.detail = ""
   end
-  -- Get Path
+
+  -- Check the Item detal is not nil and not an empty string and not includes 'Auto-import' and return defaul
+  -- item detail
   if
     entry.completion_item.detail ~= nil
     and entry.completion_item.detail ~= ""
@@ -43,27 +50,32 @@ local improved_menu = function(menu, entry, lsptext)
   then
     return "\t" .. entry.completion_item.detail
   end
-  -- Get default menu
+
+  -- Check that the menu is not nil and set a default lsp item info
   if menu ~= nil then
     return lsptext .. "\t" .. menu
   end
+
+  -- If all cases not match returns lsptext insted
   return lsptext
 end
 
----Max Width and replace with ellipsis
+---Reduce the lenght of the item, according to the `max_width`
 ---@param item string
 ---@param max_width integer
 ---@param icon_ellipsis string
 ---@return string
 local define_max_width = function(item, max_width, icon_ellipsis)
+  -- Check if the max width is not 0 and item length is more than `max_width`
   if max_width ~= 0 and #item > max_width then
     return string.sub(item, 1, max_width) .. icon_ellipsis
   end
+
+  -- If the length is less than max_width only return the normal text without ellipsis
   return item
 end
 
--- NOTE: TRY TO ALING LSP
-
+-- Format menu to Aling with (LSP)
 -- local formatedmenu = function(menu, entry, max_width, text, icon)
 --   local textwidth = 10
 --   menu = improved_menu(menu, entry)
@@ -80,6 +92,21 @@ local icons = require "core.icons"
 
 local kind_icons = icons.kind
 
+local opts_cmp = {
+  formatting = {
+    max_width_menu = 30,
+    max_width_abbr = 30,
+    ellipsis = icons.ui.Ellipsis,
+    hl_group = "Comment",
+    duplicates = {
+      buffer = 1,
+      path = 1,
+      nvim_lsp = 0,
+      luasnip = 1,
+    },
+  },
+}
+
 cmp.setup {
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
@@ -89,12 +116,19 @@ cmp.setup {
     fields = { "kind", "abbr", "menu" },
     expandable_indicator = true,
     format = function(entry, vim_item)
-      local max_width_menu = 30
-      local max_width_abbr = 30
-      local ellipsis = icons.ui.Ellipsis
-      vim_item.kind = kind_icons[vim_item.kind]
-      vim_item.menu_hl_group = "Comment"
+      --Creating variables for forrmating
+      local max_width_menu = opts_cmp.formatting.max_width_menu
+      local max_width_abbr = opts_cmp.formatting.max_width_abbr
+      local ellipsis = opts_cmp.formatting.ellipsis
       local menu = improved_menu(vim_item.menu, entry, "(LSP)")
+
+      -- Set up Icons
+      vim_item.kind = kind_icons[vim_item.kind]
+
+      -- Set up hl group for cmp menu items
+      vim_item.menu_hl_group = opts_cmp.formatting.hl_group
+
+      -- Set up Menu Messages for items
       vim_item.menu = ({
         nvim_lsp = menu,
         nvim_lua = "(Lua)",
@@ -103,28 +137,23 @@ cmp.setup {
         luasnip = "(Snippet)",
         latex_symbols = "(Latex)",
       })[entry.source.name]
-      vim_item.dup = ({
-        buffer = 1,
-        path = 1,
-        nvim_lsp = 0,
-        luasnip = 1,
-      })[entry.source.name] or 0
-      -- Reduce menu
+
+      -- Set up Duplicates
+      ---@diagnostic disable-next-line: assign-type-mismatch
+      vim_item.dup = opts_cmp.formatting.duplicates[entry.source.name] or 0
+
+      -- Reduce width of menu items and replace excedent with ellipsis for menu and abbr fields
       vim_item.abbr = define_max_width(vim_item.abbr, max_width_abbr, ellipsis)
       vim_item.menu = define_max_width(vim_item.menu, max_width_menu, ellipsis)
+
       return vim_item
     end,
   },
   window = {
+    ---@diagnostic disable-next-line: assign-type-mismatch
     documentation = false,
-    -- documentation = {
-    --   border = "rounded",
-      -- winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-      -- winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-    -- },
     completion = {
       border = "rounded",
-      -- winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
       winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
     },
   },
@@ -181,6 +210,7 @@ cmp.setup {
     {
       name = "nvim_lsp",
       entry_filter = function(entry, ctx)
+        -- Disable LSP snippets for Java
         local kind = require("cmp.types").lsp.CompletionItemKind[entry:get_kind()]
         if kind == "Snippet" and ctx.prev_context.filetype == "java" then
           return false
@@ -202,20 +232,6 @@ cmp.setup {
     },
     {
       name = "luasnip",
-    },
-  },
-  sorting = {
-    priority_weight = 2,
-    comparators = {
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
-      cmp.config.compare.recently_used,
-      cmp.config.compare.locality,
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
     },
   },
 }

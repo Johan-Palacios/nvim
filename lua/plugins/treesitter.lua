ab093748b0fbcb9f649ac885f3a9ce2329661365
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-10-13 23:05:25 -0600

    refactor: Remove comments

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -55,7 +55,7 @@ return {
         },
         ---@diagnostic disable-next-line: unused-local
         disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
+          local max_filesize = 100 * 1024
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
             return true

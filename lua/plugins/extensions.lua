200b3a202eb0d9a9ab015488f3b224498fa5331a
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-10-13 23:04:18 -0600

    feat: add config in `lsp.config.java` for Commit `cdb0c0ea`

--- lua/plugins/extensions.lua
+++ lua/plugins/extensions.lua
@@ -2,6 +2,13 @@ return {
   -- LANGUAGE TOOL
   { "simrat39/rust-tools.nvim", ft = "rust" },
   {
+    "mfussenegger/nvim-jdtls",
+    ft = "java",
+    config = function()
+      require "lsp.tools.java"
+    end,
+  },
+  {
     "p00f/clangd_extensions.nvim",
     ft = { "c", "cpp", "objc", "objcpp", "cuda" },
     config = function()

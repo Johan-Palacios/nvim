bf4e99dbfbf8fabc792ce5a5b873d2a2d3b519c9
Johan Palacios <johanpalacios.rivera@gmail.com>
2025-03-20 19:33:18 -0600

    feat: add astro typescript support

--- lua/plugins/completition.lua
+++ lua/plugins/completition.lua
@@ -274,6 +274,7 @@ return {
         "rafamadriz/friendly-snippets",
         config = function()
           -- require'luasnip'.filetype_extend("Dockerfile", {"Dockerfile.dev"})
+          require("luasnip").filetype_extend("astro", { "typescript", "html" })
           require("luasnip.loaders.from_vscode").lazy_load()
         end,
       },
